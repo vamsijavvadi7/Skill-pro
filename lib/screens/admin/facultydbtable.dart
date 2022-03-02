ee1d34c6fb6774f4766d8deb80ee1f86f192fcf7
JyothiAdabala <68616514+JyothiAdabala@users.noreply.github.com>
2022-03-02 17:02:05 +0530

    Update facultydbtable.dart

--- lib/screens/admin/facultydbtable.dart
+++ lib/screens/admin/facultydbtable.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
 
+class FacultyDb extends StatelessWidget {
+  const FacultyDb({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("FacultyDbTable"),
+      ),
+      body: Container(),
+      floatingActionButton: buildMessageButton(),
+    );
+  }
+
+  Widget buildMessageButton() => FloatingActionButton.extended(
+      onPressed: () {},
+      label: Text("Update")
+  );
+
+  Widget buildNavigateButton() => FloatingActionButton(
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(12),
+    ),
+      onPressed: () {},
+    );
+}
