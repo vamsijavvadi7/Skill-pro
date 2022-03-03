56a3cd30a5ca5a41998a6571afbf28f876c6d089
JyothiAdabala <68616514+JyothiAdabala@users.noreply.github.com>
2022-03-03 11:34:14 +0530

    Update competencies.dart

--- lib/screens/faculty/dashboard/competencies.dart
+++ lib/screens/faculty/dashboard/competencies.dart
@@ -13,40 +13,8 @@ import 'package:get/get.dart';
 // import 'dart:async';
 // import 'dart:convert';
 // import 'dart:html';
+import 'package:firstskillpro/screens/faculty/dashboard/extra.dart';
 
-class Competencies {
-  String name;
-  String regno;
-  int self;
-  int faculty;
-  int competencynum;
-  String competencyname;
-
-  Competencies(
-      {required this.name,
-        required this.regno,
-        required this.self,
-        required this.faculty,
-        required this.competencynum,
-        required this.competencyname});
-
-  factory Competencies.fromJson(Map<String, dynamic> json) {
-    return Competencies(
-        name: json['name'] as String,
-        regno: json['regno'] as String,
-        self: json['self'] as int,
-        faculty: json['faculty'] as int,
-        competencynum: json['competencynum'] as int,
-        competencyname: json['competencyname'] as String);
-  }
-}
-
-List<Competencies> parseCompetencies(String responseBody) {
-  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
-  List<Competencies> competenciesList =
-  parsed.map<Competencies>((json) => Competencies.fromJson(json)).toList();
-  return competenciesList;
-}
 
 Future<List<Competencies>> fetchCompetencies(http.Client client,int i) async {
   final response = await client.get(Uri.parse(
@@ -62,179 +30,40 @@ Future<CompetenciesDataGridSource> getCompetenciesGridSource(int i) async {
   return CompetenciesDataGridSource(competencieslist);
 }
 
-//GridView to display the data as table
-List<GridColumn> getColumn() {
-  return <GridColumn>[
-    GridColumn(
-      columnWidthMode: ColumnWidthMode.auto,
-      columnName: 'RegNo',
-      label: Container(
-        padding: EdgeInsets.all(8),
-        alignment: Alignment.center,
-        child: Text('RegNo', style: poppins),
-      ),
-    ),
-    GridColumn(
-      columnName: 'Name',
-      label: Container(
-        padding: EdgeInsets.all(8),
-        alignment: Alignment.center,
-        child: Text(
-          'Name',
-          style: poppins,
-        ),
-      ),
-    ),
-    GridColumn(
-      columnName: 'Self',
-      label: Container(
-        padding: EdgeInsets.all(8),
-        alignment: Alignment.center,
-        child: Text(
-          'Self',
-          style: poppins,
-        ),
-      ),
-    ),
-    GridColumn(
-      columnName: 'Faculty',
-      label: Container(
-        padding: const EdgeInsets.all(8),
-        alignment: Alignment.center,
-        child: Text(
-          'Faculty',
-          style: poppins,
-        ),
-      ),
-    ),
-  ];
-}
-
-class CompetenciesDataGridSource extends DataGridSource {
-  CompetenciesDataGridSource(this.competenciesList) {
-    buildDataGridRow();
-  }
-  late List<Competencies> competenciesList;
-  late List<DataGridRow> dataGridRows;
-
-  @override
-  DataGridRowAdapter? buildRow(DataGridRow row) {
-    // TODO: implement buildRow
-    return DataGridRowAdapter(color: Colors.white, cells: [
-      Text(
-        row.getCells()[0].value,
-        style: poppins,
-      ),
-      Text(
-        row.getCells()[1].value,
-        style: poppins,
-      ),
-      TextButton(
-        onPressed: () {},
-        child: Text(
-          row.getCells()[2].value.toString(),
-          style: poppins,
-        ),
-      ),
-      Text(
-        row.getCells()[3].value.toString(),
-        style: poppins,
-      ),
-    ]);
-  }
-
-  @override
-  // TODO: implement rows
-  List<DataGridRow> get rows => dataGridRows;
-  void buildDataGridRow() {
-    dataGridRows = competenciesList.map<DataGridRow>((dataGridRow) {
-      return DataGridRow(cells: [
-        DataGridCell<String>(columnName: 'RegNo', value: dataGridRow.regno),
-        DataGridCell<String>(columnName: 'Name', value: dataGridRow.name),
-        DataGridCell<int>(columnName: 'Self', value: dataGridRow.self),
-        DataGridCell<int>(columnName: 'Faculty', value: dataGridRow.faculty),
-      ]);
-    }).toList(growable: true);
-  }
-}
 var id = [4,14,24,34,44,84,94,104,114,124,134];
 class CompetenciesList extends StatelessWidget {
-  var i=0;
+
+  TextEditingController textFieldController = TextEditingController();
+
   CompetenciesList({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
 
     return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            // This is for the user details
-            // 1.Profile Picture from gamil
-            // 2.Name of the user
-            // 3.Email of the user
-            GetFacultyDetails(
-                controller:
-                controller), //Returns details of faculty. Ex:Name,Email,Role,ProfilePicture
-
-            //To create line between user details and competencies
-            const Divider(
-              height: 20.00,
-              thickness: 2.00,
-              color: Colors.black,
-            ),
-            Expanded(
-                child:Column(
-                  children:<Widget>[
-                    for(int i=0;i<id.length;i++)
-                    FutureBuilder(
-                      future: getCompetenciesGridSource(id[i]),
-                      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-                          if (snapshot.hasData) {
-                            return Expanded(
-                                child: ListView(
-                                  physics: ScrollPhysics(),
-                                  padding: const EdgeInsets.all(2),
-                                  children: <Widget>[
-                                      ClipRRect(
-                                        borderRadius: BorderRadius.circular(15),
-                                        child: Container(
-                                          margin: EdgeInsets.fromLTRB(0.0, 5, 0.0, 5),
-                                          color: secondaryColor,
-                                          child: ExpansionTile(
-                                            collapsedBackgroundColor: secondaryColor,
-                                            title: Text('Competency 1',
-                                                style: GoogleFonts.poppins(color: Colors.white)),
-                                            leading: CircleAvatar(
-                                                backgroundColor: Colors.white,
-                                                child: Text(
-                                                  'C1',
-                                                  style: GoogleFonts.baloo(),
-                                                )),
-                                            children: <Widget>[
-                                              SfDataGrid(
-                                                  columnWidthMode: ColumnWidthMode.fill,
-                                                  allowSorting: true,
-                                                  source: snapshot.data,
-                                                  verticalScrollPhysics: NeverScrollableScrollPhysics(),
-                                                  columns: getColumn())
-                                            ],
-                                            iconColor: Colors.white,
-                                            collapsedIconColor: Colors.white,
-                                          ),
-                                        ),
-                                      ),
-                                  ],
-                                ));
-                          } else {
-                            return const Center(
-                              child: CircularProgressIndicator(),
-                            );
-                          }
-                        },
-                    ),
-                  ],
-                )),
-          ],
-        ));
+      child: Column(
+        children: <Widget>[
+          GetFacultyDetails(
+              controller:
+              controller),
+          const Divider(
+            height: 20.00,
+            thickness: 2.00,
+            color: Colors.black,
+          ),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                for(int i=0;i<id.length;i++)
+                  ElevatedButton(
+                      onPressed: (){
+                        Navigator.push(context,MaterialPageRoute(builder: (context) => Extra()));
+                      },
+                      child: Text('Competency ${id[i]}',
+                          style: GoogleFonts.poppins(color: Colors.white))),
+              ],
+          )),
+        ],
+      ),
+      );
   }
 }
