2b675d24102b5deaacdfa9008e93ee97ebd6ae9d
JyothiAdabala <68616514+JyothiAdabala@users.noreply.github.com>
2022-03-05 13:04:12 +0530

    Update get_details.dart

--- lib/screens/faculty/dashboard/get_details.dart
+++ lib/screens/faculty/dashboard/get_details.dart
@@ -1,86 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:math';
+import 'package:firstskillpro/screens/admins/sample_admin.dart';
+import '../../login/login_controller.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:firstskillpro/screens/login/login_controller.dart';
-class GetFacultyDetails extends StatefulWidget {
-  const GetFacultyDetails({
-    Key? key,
-    required this.controller,
-  }) : super(key: key);
 
-  final LoginController controller;
+Future<Role> getData(String email) async {
+  final response = await http
+      .get(Uri.parse('https://api421.herokuapp.com/login/email/$email'));
 
-  @override
-  State<GetFacultyDetails> createState() => _GetFacultyDetailsState();
-}
-
-class _GetFacultyDetailsState extends State<GetFacultyDetails> {
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 5, 10 ,0),
-                    child: CircleAvatar(
-                      backgroundImage: Image.network(
-                              widget.controller.googleAccount.value?.photoUrl ?? '')
-                          .image,
-                      radius: 35,
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Name: ${widget.controller.googleAccount.value?.displayName ?? ''}',
-                        style: GoogleFonts.poppins(),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Email: ${widget.controller.googleAccount.value?.email ?? ''}',
-                            textAlign: TextAlign.justify,
-                            style: GoogleFonts.poppins(),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Role: Surgeon',
-                            textAlign: TextAlign.start,
-                            style: GoogleFonts.poppins(),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Role.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
   }
 }
+
+class Role {
+  late String name;
+ late String role;
+
+  Role({required this.name, required this.role});
+
+  Role.fromJson(Map<String, dynamic> json) {
+    name: json['name'] as String;
+    role: json['role'] as String;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['name'] = name;
+    data['role'] = role;
+    return data;
+  }
+}
+var sp="";
+
+class DetailWidget extends StatefulWidget{
+  @override
+  _LoginWidgetState createState() => _LoginWidgetState();
+}
+
+class _LoginWidgetState extends State<DetailWidget> {
+  //String email = controller.googleAccount.value?.email ?? '';
+  late Future<Role> fetchRole;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Obx(() {
+      //controller.googleAccount.value!.email
+        fetchRole = getData("vamsijavvadi@gmail.com");
+        return FutureBuilder<Role>(
+            future: fetchRole,
+            builder: (context, snapshot) {
+              sp = snapshot.data!.role;
+              if (snapshot.hasData) {
+                return ListView(
+                    children: <Widget>[
+                      Text(snapshot.data!.name)
+                      ,
+                      Text(snapshot.data!.role),
+                    ]);
+
+              } else if (snapshot.hasError) {
+                return Scaffold(
+                    body: Center(child:(Text('${snapshot.error}'))),
+                    );
+
+              }
+              else{
+                return Text("sdsdsds");
+              }
+            });
+      }
+    );
+  }
+  }
